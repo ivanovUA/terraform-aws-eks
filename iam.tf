f6e93a8ff5c7747f1427114f73ad589feccc581a
Alex Ivanov <ivanov.ua@ukr.net>
2021-09-02 17:23:51 +0300

    control plane iam role

---
+++ iam.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "control_plane" {
+    name = format("%s-cp", local.name)
+    tags = merge(local.default-tags, var.tags)
+    assume_role_policy = jsonencode({
+        Version : "2012-10-17",
+        Statement : [
+        {
+            Effect : "Allow",
+            Principal : {
+            Service : "eks.amazonaws.com"
+            },
+            Action : "sts:AssumeRole"
+        }
+        ]
+    })
+}
