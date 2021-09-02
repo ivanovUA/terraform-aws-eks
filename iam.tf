270542351a67733ea22dbe8e72bdd8cecda5b494
Alex Ivanov <ivanov.ua@ukr.net>
2021-09-02 17:32:58 +0300

    added policy to control plane

--- iam.tf
+++ iam.tf
@@ -14,3 +14,14 @@ resource "aws_iam_role" "control_plane" {
         ]
     })
 }
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    role = aws_iam_role.control_plane.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+    role = aws_iam_role.control_plane.name
+}
+
